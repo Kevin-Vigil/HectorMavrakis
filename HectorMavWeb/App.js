f5512ef9ef1cc363846970bba6110ac08d786889
Kevin Vigil <thekevinvigil@gmail.com>
2023-11-01 03:45:01 -0500

    Installed web dependencies

--- HectorMavWeb/App.js
+++ HectorMavWeb/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World, Hector Mavrakis Website is on the way!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
