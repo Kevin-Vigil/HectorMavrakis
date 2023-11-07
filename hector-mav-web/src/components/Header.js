ee895e9393a8eb20b1ad31441f0e98695d6e6e01
Kevin Vigil <thekevinvigil@gmail.com>
2023-11-07 03:12:45 -0600

    Created header

---
+++ hector-mav-web/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Header(){
+    return(
+        <>
+            <nav className='header'>
+                <div className='header-container'>
+                    <Link to='/' className='header-logo'>
+                        HectorMavrakis <i className='fab fa-typo3'/>
+                    </Link>
+                    <Link>
+                        
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
