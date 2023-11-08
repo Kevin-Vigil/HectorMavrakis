8bfca50ebee60ad57c558b6ef1746fc67aac5cd1
Kevin Vigil <thekevinvigil@gmail.com>
2023-11-07 23:22:39 -0600

    Created css

--- hector-mav-web/src/components/Header.js
+++ hector-mav-web/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function Header(){
+    
     return(
         <>
             <nav className='header'>
@@ -14,6 +15,13 @@ function Header(){
                     </Link>
                 </div>
             </nav>
+        </>
+    )
+}
+
+function MobileHeader(){
+    return(
+        <>
         </>
     )
 }
