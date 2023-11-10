f9bd16216c4044614c296adc151126e24a6b17d8
Kevin Vigil <thekevinvigil@gmail.com>
2023-11-10 01:52:13 -0600

    created responsive header in Header

--- hector-mav-web/src/App.js
+++ hector-mav-web/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' exact />
         </Routes>
--- hector-mav-web/src/components/Header.js
+++ hector-mav-web/src/components/Header.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 
+
+
 function Header(){
+    const [mobile, setMobile] = useState(getMobile())
+
+    useEffect(() => {
+        function handleWindowResize() {
+          setMobile(getMobile());
+        }
     
+        window.addEventListener('resize', handleWindowResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleWindowResize);
+        };
+      }, []
+    );
+
     return(
         <>
+            {mobile? (
+            <MobileHeader/>):(
             <nav className='header'>
                 <div className='header-container'>
-                    <Link to='/' className='header-logo'>
-                        HectorMavrakis <i className='fab fa-typo3'/>
-                    </Link>
-                    <Link>
-                        
+                    <Link to="/" className='header-logo'>
+                        innerWidth: {mobile.innerWidth} <i className='fab fa-typo3'/>
                     </Link>
                 </div>
             </nav>
+            )}
         </>
     )
 }
 
+function getMobile() {
+    const innerWidth = window.innerWidth;
+    return (innerWidth < 720);
+  }
+
 function MobileHeader(){
     return(
         <>
+            <div>
+                test
+            </div>
         </>
     )
 }
