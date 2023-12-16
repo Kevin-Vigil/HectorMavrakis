549897ce41677523c40fb11f01b82ce85b143c63
sporti <thekevinvigil@gmail.com>
2023-12-16 04:46:50 -0600

    carousel created, animation complete. darkmode in progress

--- hector-mav-web/src/components/Carousel.js
+++ hector-mav-web/src/components/Carousel.js
@@ -1,5 +1,15 @@
 import './Carousel.css';
+// Light mode assets
 import { ReactComponent as Ups } from '../assets/ups.svg';
+import { ReactComponent as Discover } from '../assets/discover.svg';
+import { ReactComponent as Keurig } from '../assets/keurig.svg';
+import { ReactComponent as Disney } from '../assets/disney.svg';
+// Dark mode assets
+import { ReactComponent as UpsD } from '../assets/upsD.svg';
+import { ReactComponent as DiscoverD } from '../assets/discoverD.svg';
+import { ReactComponent as KeurigD } from '../assets/keurigD.svg';
+import { ReactComponent as DisneyD } from '../assets/disneyD.svg';
+import './Animations.css'
 
 export default function Carousel(props){
     return(
@@ -9,9 +19,44 @@ export default function Carousel(props){
                 <div className='subtitle'>Some of the brands that I've worked with.</div>
             </div>
             <div className='carousel'>
-                <div className='card-box ups-card-1'>
-                    <Ups className="caro-svg"/>
+                <div className='card-box'>
+                    <Ups className="ups-svg"/>
+                    <UpsD className="ups-d-svg"/>
                 </div>
+                <div className='card-box'>
+                    <Disney className="disney-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Keurig className="keurig-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Discover className="discover-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Ups className="ups-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Disney className="disney-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Keurig className="keurig-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Discover className="discover-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Ups className="ups-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Disney className="disney-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Keurig className="keurig-svg"/>
+                </div>
+                <div className='card-box'>
+                    <Discover className="discover-svg"/>
+                </div>
+                
 
             </div>
         </div>
