8bb5ac946faff410769b1f60b8792867d158f839
sporti <thekevinvigil@gmail.com>
2023-12-15 22:51:26 -0600

    svg added

--- hector-mav-web/src/components/Carousel.js
+++ hector-mav-web/src/components/Carousel.js
@@ -1,4 +1,5 @@
 import './Carousel.css';
+import { ReactComponent as Ups } from '../assets/ups.svg';
 
 export default function Carousel(props){
     return(
@@ -8,7 +9,9 @@ export default function Carousel(props){
                 <div className='subtitle'>Some of the brands that I've worked with.</div>
             </div>
             <div className='carousel'>
-                
+                <div className='card-box ups-card-1'>
+                    <Ups className="caro-svg"/>
+                </div>
 
             </div>
         </div>
