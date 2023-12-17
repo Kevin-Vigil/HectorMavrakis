0f141ae877bdcca4ee5d6bbfad82906a362d4d03
sporti <thekevinvigil@gmail.com>
2023-12-17 04:23:10 -0600

    Minor changes to Projects component

--- hector-mav-web/src/components/Projects.js
+++ hector-mav-web/src/components/Projects.js
@@ -2,10 +2,32 @@ import React from "react";
 import './Projects.css';
 
 export default function Projects(){
-    <>
+    return(
+    <div id="projects-container">
         <div id="projects-list-textbox">
             <div id="projects-list-header">PROJECTS</div>
-            <div id="projects-list-statement"></div>
+            <div id="projects-list-statement">Take a look at what I've designed.</div>
         </div>
-    </>
+        <div id="projects-list-container">
+            <div id="row-1" className="projects-box-container">
+                <div id="walmart-container">
+                    <div id="walmart-textbox">
+
+                    </div>
+                    <div id="walmart-phone-demo">
+                        
+                    </div>
+                </div>
+            </div>
+            <div id="row-2" className="projects-box-container">
+                <div id="quantify-container">
+
+                </div>
+                <div id="big-o-tires-container">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
