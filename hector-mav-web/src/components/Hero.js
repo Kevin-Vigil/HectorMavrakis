a38a5f70b40d57ad2ec92b9aa4999d243a7f19d1
sporti <thekevinvigil@gmail.com>
2023-12-18 13:08:31 -0600

    Format changes in Hero

--- hector-mav-web/src/components/Hero.js
+++ hector-mav-web/src/components/Hero.js
@@ -4,52 +4,52 @@ import './Animations.css'
 import { Link } from "react-router-dom";
 import phoneLight from '../assets/lightPhoneDemo.png'
 import phoneDark from "../assets/darkPhoneDemo.png"
-import {ReactComponent as Arrow} from '../assets/Vectorarrow.svg'
+import { ReactComponent as Arrow } from '../assets/Vectorarrow.svg'
 
-export default function Hero(props){
-    
-    function load(){
-        document.documentElement.classList.toggle("loadin", true)
-        
-        const aniEnd = document.querySelector(".scroll-down")
-        aniEnd.addEventListener("animationend", (event) =>{
-            if (event.animationName === 'load-in')
-                document.documentElement.classList.toggle("loadin", false)
-        })
+export default function Hero(props) {
 
-        
+  function load() {
+    document.documentElement.classList.toggle("loadin", true)
+
+    const aniEnd = document.querySelector(".scroll-down")
+    aniEnd.addEventListener("animationend", (event) => {
+      if (event.animationName === 'load-in')
+        document.documentElement.classList.toggle("loadin", false)
+    })
 
 
 
-    }
 
-    document.addEventListener("DOMContentLoaded",load)
-    
 
-    return(
-        <>
-            <div className="hero-container">
-                <div className="scroll-down">
-                    <div className="line1"/>
-                    <div className="scroll-text">SCROLL DOWN</div>
-                    <div className="arrow">
-                        <div className="line2"/>
-                        <div className="arrowhead"/>
-                    </div>
-                </div>
-                <div className="hero-content">
-                    <div className="statement">I DESIGN WHAT PEOPLE</div>
-                    <div className="envision">ENVISION</div>
-                    <div className="hero-purpose">Alleviating end-users' frustrations through unique solutions.</div>
-                    <Link to='#projects'className="view-projects-container">
-                        <div className="view-projects-text">View projects</div>
-                        <Arrow className='view-projects-arrow-svg'/>
-                    </Link>
-                    <div className="hero-phone-img-container">
-                        <img loading='lazy' className='phone-demo-img' src={phoneLight}/>
-                    </div>
-                </div>
-            </div>  
-        </>
-    );    
+  }
+
+  document.addEventListener("DOMContentLoaded", load)
+
+
+  return (
+    <>
+      <div className="hero-container">
+        <div className="scroll-down">
+          <div className="line1" />
+          <div className="scroll-text">SCROLL DOWN</div>
+          <div className="arrow">
+            <div className="line2" />
+            <div className="arrowhead" />
+          </div>
+        </div>
+        <div className="hero-content">
+          <div className="statement">I DESIGN WHAT PEOPLE</div>
+          <div className="envision">ENVISION</div>
+          <div className="hero-purpose">Alleviating end-users' frustrations through unique solutions.</div>
+          <Link to='#projects' className="view-projects-container">
+            <div className="view-projects-text">View projects</div>
+            <Arrow className='view-projects-arrow-svg' />
+          </Link>
+          <div className="hero-phone-img-container">
+            <img loading='lazy' className='phone-demo-img' src={phoneLight} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 } 
