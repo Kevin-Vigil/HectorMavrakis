dba09d9ea2d209312ac398de615394b4fe5bc1ba
sporti <thekevinvigil@gmail.com>
2023-12-18 13:06:42 -0600

    Formatting change in App

--- hector-mav-web/src/App.js
+++ hector-mav-web/src/App.js
@@ -9,13 +9,13 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <div className='container'>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
