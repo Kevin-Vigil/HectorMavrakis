b171bf04a53fdd9d27b5a6bb80c3101c41401033
sporti <thekevinvigil@gmail.com>
2023-12-18 13:11:39 -0600

    Format change in Footer

--- hector-mav-web/src/components/Footer.js
+++ hector-mav-web/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Footer(){
-    return(
-        <>
-            <nav className='footer'>
-                <div className='footer-container'>
-                    <Link to='/' className='footer-logo'>
-                        HectorMavrakis <i className='fab fa-typo3'/>
-                    </Link>
-                </div>
-            </nav>
-        </>
-    )
+function Footer() {
+  return (
+    <>
+      <nav className='footer'>
+        <div className='footer-container'>
+          <Link to='/' className='footer-logo'>
+            HectorMavrakis <i className='fab fa-typo3' />
+          </Link>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Footer;
