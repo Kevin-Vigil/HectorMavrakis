4d2a255e5e14c48282cf8a68e82b1ae22ae4f3b4
sporti <thekevinvigil@gmail.com>
2023-12-22 05:51:30 -0600

    Hero changes

--- hector-mav-web/src/components/Hero.js
+++ hector-mav-web/src/components/Hero.js
@@ -30,33 +30,31 @@ export default function Hero(props) {
     load();
   });
   return (
-    <>
-      <div id="hero-container">
-        <div id="scroll-down">
-          <div id="line1" />
-          <div id="scroll-text">SCROLL DOWN</div>
-          <div id="arrow">
-            <div id="line2" />
-            <div id="arrowhead" />
-          </div>
-        </div>
-        <div id="hero-content">
-          <div id="hero-textbox">
-            <div id="statement" >I DESIGN WHAT PEOPLE</div>
-            <div id="envision"  >ENVISION</div>
-            <div id="hero-purpose" >Alleviating end-users' frustrations through unique solutions.</div>
-          </div>
-          <a href='#projects' id="cta" >
-            <div id="hero-cta-textbox">
-              <div className="view-projects-text">View projects</div>
-              <Arrow className='view-projects-arrow-svg' />
-            </div>
-          </a>
-          <div id="hero-phone-img-container" >
-            <img loading='lazy' className='phone-demo-img' src={phoneLight} />
-          </div>
-        </div>
+  <div id="hero-container">
+    <div id="scroll-down">
+      <div id="line1" />
+      <div id="scroll-text">SCROLL DOWN</div>
+      <div id="arrow">
+        <div id="line2" />
+        <div id="arrowhead" />
       </div>
-    </>
+    </div>
+    <div id="hero-content">
+      <div id="hero-textbox">
+        <div id="statement" >I DESIGN WHAT PEOPLE</div>
+        <div id="envision"  >ENVISION</div>
+        <div id="hero-purpose" >Alleviating end-users' frustrations through unique solutions.</div>
+      </div>
+      <a href='#projects' id="cta" >
+        <div id="hero-cta-textbox">
+          <div className="view-projects-text">View projects</div>
+          <Arrow className='view-projects-arrow-svg' />
+        </div>
+      </a>
+      <div id="hero-phone-img-container" >
+        <img loading='lazy' className='phone-demo-img' src={phoneLight} />
+      </div>
+    </div>
+  </div>
   );
 } 
