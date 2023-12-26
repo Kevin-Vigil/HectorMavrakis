9d2d675a825d2dbd3da33e02985c283c8063d623
sporti <thekevinvigil@gmail.com>
2023-12-26 03:02:52 -0600

    Removed import from Quantify page

--- hector-mav-web/src/pages/Quantify.js
+++ hector-mav-web/src/pages/Quantify.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ProjectTemplate from "../components/ProjectTemplate";
 import Footer from "../components/Footer";
-import Hero from "../components/Hero";
 
 import './Quantify.css'
 
