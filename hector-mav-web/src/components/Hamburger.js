f5798d10a556ba9a65e60c8fa050e637584435a9
sporti <thekevinvigil@gmail.com>
2023-12-31 17:20:05 -0600

    Created Hamburger component

---
+++ hector-mav-web/src/components/Hamburger.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hamburger(props){
+    <div className="hamburger">
+        <div className="burger burger1"/>
+        <div className="burger burger2"/>
+        <div className="burger burger3"/>
+    </div>
+}
