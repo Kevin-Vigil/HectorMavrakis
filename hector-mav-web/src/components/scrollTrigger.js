4088b2c51c50aa757721ee96ae83740df5eb34e8
sporti <thekevinvigil@gmail.com>
2024-01-03 02:34:39 -0600

    Fixed one time observer in Scroll Trigger component

--- hector-mav-web/src/components/scrollTrigger.js
+++ hector-mav-web/src/components/scrollTrigger.js
@@ -5,35 +5,37 @@ export default function scrollTrigger(selector, options, constant) {
   let eventListenerArr = document.querySelectorAll(selector)
   eventListenerArr = Array.from(eventListenerArr)
   eventListenerArr.forEach(el => {
-    constant?
-    addConstObserver(el, options):
-    addObserver(el, options);
+    constant ?
+      addConstObserver(el, options) :
+      addObserver(el, options);
   })
 }
 
 function addConstObserver(el, options) {
   let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry =>{
+    entries.forEach(entry => {
       console.log("Test: " + entry.target.classList + " \tFor: " + entry.target.id)
-      const intersecting=entry.isIntersecting
-      intersecting? entry.target.classList.add("active-comp"): entry.target.classList.remove("active-comp")
+      const intersecting = entry.isIntersecting
+      intersecting ? entry.target.classList.add("active-comp") : entry.target.classList.remove("active-comp")
 
     })
   },
-  options);
+    options);
   observer.observe(el)
 }
 
 function addObserver(el, options) {
   let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry =>{
-      console.log("Test: " + entry.target.classList + " \tFor: " + entry.target.id)
-      const intersecting=entry.isIntersecting
-      intersecting? entry.target.classList.add("active-comp"): entry.target.classList.remove("active-comp")
-
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        console.log("Test: " + entry.target.classList + " \tFor: " + entry.target.id)
+        entry.target.classList.add('active-comp')
+        entry.target.classList.remove('active-listener')
+        observer.unobserve(entry.target)
+      }
     })
   },
-  options);
+    options);
   observer.observe(el)
 }
 
