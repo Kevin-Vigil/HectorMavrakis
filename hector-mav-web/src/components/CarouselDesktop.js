4defc94864737ed88055c399304d9968a5a576a1
sporti <thekevinvigil@gmail.com>
2024-01-03 13:07:11 -0600

    Full overhaul of Carousel and Carousel Logos

--- hector-mav-web/src/components/CarouselDesktop.js
+++ hector-mav-web/src/components/CarouselDesktop.js
@@ -1,16 +1,24 @@
 import './CarouselDesktop.css';
 // Light mode assets
-import { ReactComponent as Ups } from '../assets/ups.svg';
-import { ReactComponent as Discover } from '../assets/discover.svg';
-import { ReactComponent as Keurig } from '../assets/keurig.svg';
-import { ReactComponent as Disney } from '../assets/disney.svg';
-import { ReactComponent as Lowes } from '../assets/lowes.svg'
+import { ReactComponent as Ups } from './../assets/companySvgs/lightMode/UPS.svg';
+import { ReactComponent as Discover } from '../assets/companySvgs/lightMode/Discover.svg';
+import { ReactComponent as Keurig } from '../assets/companySvgs/lightMode/Keurig.svg';
+import { ReactComponent as Disney } from '../assets/companySvgs/lightMode/shopDisney.svg';
+import { ReactComponent as Lowes } from '../assets/companySvgs/lightMode/Lowes.svg'
+import { ReactComponent as BigO } from '../assets/companySvgs/lightMode/BigOTires.svg'
+import { ReactComponent as McDonalds } from '../assets/companySvgs/lightMode/McDonalds.svg'
+import { ReactComponent as Verizon } from '../assets/companySvgs/lightMode/Verizon.svg'
+import { ReactComponent as Walgreens } from '../assets/companySvgs/lightMode/Walgreens.svg'
 // Dark mode assets
-import { ReactComponent as UpsD } from '../assets/upsD.svg';
-import { ReactComponent as DiscoverD } from '../assets/discoverD.svg';
-import { ReactComponent as KeurigD } from '../assets/keurigD.svg';
-import { ReactComponent as DisneyD } from '../assets/disneyD.svg';
-import { ReactComponent as LowesD } from '../assets/lowesD.svg'
+import { ReactComponent as UpsD } from './../assets/companySvgs/darkMode/UPSD.svg';
+import { ReactComponent as DiscoverD } from '../assets/companySvgs/darkMode/DiscoverD.svg';
+import { ReactComponent as KeurigD } from '../assets/companySvgs/darkMode/KeurigD.svg';
+import { ReactComponent as DisneyD } from '../assets/companySvgs/darkMode/shopDisneyD.svg';
+import { ReactComponent as LowesD } from '../assets/companySvgs/darkMode/LowesD.svg'
+import { ReactComponent as BigOD } from '../assets/companySvgs/darkMode/BigOTiresD.svg'
+import { ReactComponent as McDonaldsD } from '../assets/companySvgs/darkMode/McDonaldsD.svg'
+import { ReactComponent as VerizonD } from '../assets/companySvgs/darkMode/VerizonD.svg'
+import { ReactComponent as WalgreensD } from '../assets/companySvgs/darkMode/WalgreensD.svg'
 
 import walgreens from '../assets/walgreens.png'
 import './Animations.css'
@@ -60,7 +68,7 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            <img loading='lazy' src={walgreens} className='walgreens-png'/>
+            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
           </div>
           <div className='card-box'>
             <Lowes className='lowes-svg'/>
@@ -83,7 +91,7 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            <img loading='lazy' src={walgreens} className='walgreens-png'/>
+            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
           </div>
           <div className='card-box'>
             <Lowes className='lowes-svg'/>
@@ -106,7 +114,7 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            <img loading='lazy' src={walgreens} className='walgreens-png'/>
+            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
           </div>
           <div className='card-box'>
             <Lowes className='lowes-svg'/>
