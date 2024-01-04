b53aa16ddbc0aa45859558fb52d3d3f37937626f
sporti <thekevinvigil@gmail.com>
2024-01-04 02:00:09 -0600

    Completion of Desktop Carousel component

--- hector-mav-web/src/components/CarouselDesktop.js
+++ hector-mav-web/src/components/CarouselDesktop.js
@@ -1,26 +1,25 @@
 import './CarouselDesktop.css';
 // Light mode assets
-import { ReactComponent as Ups } from './../assets/companySvgs/lightMode/UPS.svg';
-import { ReactComponent as Discover } from '../assets/companySvgs/lightMode/Discover.svg';
-import { ReactComponent as Keurig } from '../assets/companySvgs/lightMode/Keurig.svg';
-import { ReactComponent as Disney } from '../assets/companySvgs/lightMode/shopDisney.svg';
-import { ReactComponent as Lowes } from '../assets/companySvgs/lightMode/Lowes.svg'
-import { ReactComponent as BigO } from '../assets/companySvgs/lightMode/BigOTires.svg'
-import { ReactComponent as McDonalds } from '../assets/companySvgs/lightMode/McDonalds.svg'
-import { ReactComponent as Verizon } from '../assets/companySvgs/lightMode/Verizon.svg'
-import { ReactComponent as Walgreens } from '../assets/companySvgs/lightMode/Walgreens.svg'
+import { ReactComponent as Ups } from '../assets/CarouselLogos/Color/UPS.svg';
+import { ReactComponent as Disney } from '../assets/CarouselLogos/Color/shopDisney.svg';
+import { ReactComponent as Discover } from '../assets/CarouselLogos/Color/Discover.svg';
+import { ReactComponent as Keurig } from '../assets/CarouselLogos/Color/Keurig.svg';
+import { ReactComponent as Lowes } from '../assets/CarouselLogos/Color/Lowe\'s.svg'
+import { ReactComponent as BigO } from '../assets/CarouselLogos/Color/BigOTires.svg'
+import { ReactComponent as McDonalds } from '../assets/CarouselLogos/Color/McDonald\'s.svg'
+import { ReactComponent as Verizon } from '../assets/CarouselLogos/Color/Verizon.svg'
+import { ReactComponent as Walgreens } from '../assets/CarouselLogos/Color/Walgreens.svg'
 // Dark mode assets
-import { ReactComponent as UpsD } from './../assets/companySvgs/darkMode/UPSD.svg';
-import { ReactComponent as DiscoverD } from '../assets/companySvgs/darkMode/DiscoverD.svg';
-import { ReactComponent as KeurigD } from '../assets/companySvgs/darkMode/KeurigD.svg';
-import { ReactComponent as DisneyD } from '../assets/companySvgs/darkMode/shopDisneyD.svg';
-import { ReactComponent as LowesD } from '../assets/companySvgs/darkMode/LowesD.svg'
-import { ReactComponent as BigOD } from '../assets/companySvgs/darkMode/BigOTiresD.svg'
-import { ReactComponent as McDonaldsD } from '../assets/companySvgs/darkMode/McDonaldsD.svg'
-import { ReactComponent as VerizonD } from '../assets/companySvgs/darkMode/VerizonD.svg'
-import { ReactComponent as WalgreensD } from '../assets/companySvgs/darkMode/WalgreensD.svg'
+import { ReactComponent as UpsD } from '../assets/CarouselLogos/B&W/UPS.svg';
+import { ReactComponent as DisneyD } from '../assets/CarouselLogos/B&W/shopDisney.svg';
+import { ReactComponent as DiscoverD } from '../assets/CarouselLogos/B&W/Discover.svg';
+import { ReactComponent as KeurigD } from '../assets/CarouselLogos/B&W/Keurig.svg';
+import { ReactComponent as LowesD } from '../assets/CarouselLogos/B&W/Lowe\'s.svg'
+import { ReactComponent as BigOD } from '../assets/CarouselLogos/B&W/BigOTires.svg'
+import { ReactComponent as McDonaldsD } from '../assets/CarouselLogos/B&W/McDonald\'s.svg'
+import { ReactComponent as VerizonD } from '../assets/CarouselLogos/B&W/Verizon.svg'
+import { ReactComponent as WalgreensD } from '../assets/CarouselLogos/B&W/Walgreens.svg'
 
-import walgreens from '../assets/walgreens.png'
 import './Animations.css'
 
 export default function CarouselDesktop(props) {
@@ -45,34 +44,24 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            <img loading='lazy' src={walgreens} className='walgreens-png'/>
+            <Walgreens className="walgreens-svg" />
+            <WalgreensD className="walgreens-d-svg" />
           </div>
           <div className='card-box'>
-            <Lowes className='lowes-svg'/>
-            <LowesD className='lowes-d-svg' />
+            <Lowes className="lowes-svg" />
+            <LowesD className="lowes-d-svg" />
           </div>
           <div className='card-box'>
-            <Ups className="ups-svg" />
-            <UpsD className="ups-d-svg" />
+            <BigO className="big-svg" />
+            <BigOD className="big-d-svg" />
           </div>
           <div className='card-box'>
-            <Disney className="disney-svg" />
-            <DisneyD className="disney-d-svg" />
+            <McDonalds className="mcdonalds-svg" />
+            <McDonaldsD className="mcdonalds-d-svg" />
           </div>
           <div className='card-box'>
-            <Keurig className="keurig-svg" />
-            <KeurigD className="keurig-d-svg" />
-          </div>
-          <div className='card-box'>
-            <Discover className="discover-svg" />
-            <DiscoverD className="discover-d-svg" />
-          </div>
-          <div className='card-box'>
-            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
-          </div>
-          <div className='card-box'>
-            <Lowes className='lowes-svg'/>
-            <LowesD className='lowes-d-svg' />
+            <Verizon className="verizon-svg" />
+            <VerizonD className="verizon-d-svg" />
           </div>
           <div className='card-box'>
             <Ups className="ups-svg" />
@@ -91,11 +80,24 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
+            <Walgreens className="walgreens-svg" />
+            <WalgreensD className="walgreens-d-svg" />
           </div>
           <div className='card-box'>
-            <Lowes className='lowes-svg'/>
-            <LowesD className='lowes-d-svg' />
+            <Lowes className="lowes-svg" />
+            <LowesD className="lowes-d-svg" />
+          </div>
+          <div className='card-box'>
+            <BigO className="big-svg" />
+            <BigOD className="big-d-svg" />
+          </div>
+          <div className='card-box'>
+            <McDonalds className="mcdonalds-svg" />
+            <McDonaldsD className="mcdonalds-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Verizon className="verizon-svg" />
+            <VerizonD className="verizon-d-svg" />
           </div>
           <div className='card-box'>
             <Ups className="ups-svg" />
@@ -114,13 +116,61 @@ export default function CarouselDesktop(props) {
             <DiscoverD className="discover-d-svg" />
           </div>
           <div className='card-box'>
-            {/* <img loading='lazy' src={walgreens} className='walgreens-png'/> */}
+            <Walgreens className="walgreens-svg" />
+            <WalgreensD className="walgreens-d-svg" />
           </div>
           <div className='card-box'>
-            <Lowes className='lowes-svg'/>
-            <LowesD className='lowes-d-svg' />
+            <Lowes className="lowes-svg" />
+            <LowesD className="lowes-d-svg" />
           </div>
-
+          <div className='card-box'>
+            <BigO className="big-svg" />
+            <BigOD className="big-d-svg" />
+          </div>
+          <div className='card-box'>
+            <McDonalds className="mcdonalds-svg" />
+            <McDonaldsD className="mcdonalds-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Verizon className="verizon-svg" />
+            <VerizonD className="verizon-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Ups className="ups-svg" />
+            <UpsD className="ups-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Disney className="disney-svg" />
+            <DisneyD className="disney-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Keurig className="keurig-svg" />
+            <KeurigD className="keurig-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Discover className="discover-svg" />
+            <DiscoverD className="discover-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Walgreens className="walgreens-svg" />
+            <WalgreensD className="walgreens-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Lowes className="lowes-svg" />
+            <LowesD className="lowes-d-svg" />
+          </div>
+          <div className='card-box'>
+            <BigO className="big-svg" />
+            <BigOD className="big-d-svg" />
+          </div>
+          <div className='card-box'>
+            <McDonalds className="mcdonalds-svg" />
+            <McDonaldsD className="mcdonalds-d-svg" />
+          </div>
+          <div className='card-box'>
+            <Verizon className="verizon-svg" />
+            <VerizonD className="verizon-d-svg" />
+          </div>
         </div>
       </div>
     </div>
