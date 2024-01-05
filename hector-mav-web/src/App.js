d914b4194bf246aacc775c9ba62e93e9e2a93d71
sporti <thekevinvigil@gmail.com>
2024-01-05 03:52:23 -0600

    Created scroll to top component

--- hector-mav-web/src/App.js
+++ hector-mav-web/src/App.js
@@ -6,15 +6,19 @@ import { Routes, Route, Router } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Quantify from './pages/Quantify';
+import ScrollToTop from './components/ScrollToTop';
 // import ProjectTemplate from './components/ProjectTemplate';
 
 
 function App() {
-  
+  // window.onbeforeunload = function () {
+  //   window.scrollTo(0, 0);
+  // }
   return (
     <div id='app'>
       <script>0</script>
       <Header />
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/quantify' element={<Quantify/>}/>
